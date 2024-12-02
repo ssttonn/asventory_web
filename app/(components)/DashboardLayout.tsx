28dcbfcad8f1cc7c2bb0796921fe80c29e0c2166
sstonn <sstonn2501@gmail.com>
2024-12-02 09:04:13 +0700

    [DEV] [Add .gitignore for IDE files, enhance DashboardLayout with dark mode and sidebar state management, and improve SideBar component functionality]

--- app/(components)/DashboardLayout.tsx
+++ app/(components)/DashboardLayout.tsx
@@ -1,19 +1,42 @@
 "use client";
 
-import React, { memo } from "react";
-import NavBar from "@/app/(components)/NavBar";
-import SideBar from "@/app/(components)/SideBar";
-import StoreProvider from "../redux";
+import React, { memo, useEffect } from "react";
+
+import StoreProvider, { useAppSelector } from "../redux";
+import SideBar from "./SideBar";
+import NavBar from "./NavBar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
       <SideBar />
-      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
+      >
         <NavBar />
         {children}
       </main>
